33ed7b79105f1676927d1ff6cbba851ff0595a5e
Justin Voye <justin_voye@yahoo.com>
2021-04-01 13:46:20 -0600

    add dropdown

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
-const btn = document.querySelector("#menuBtn");
-const list = document.querySelector("#menuList");
+const btn = document.querySelector('#menuBtn');
+const list = document.querySelector('#menuList');
 
-btn.addEventListener("click", function () {
-  if (list.classList.contains("show")) {
-    list.classList.remove("show");
+btn.addEventListener('click', function () {
+  if (list.classList.contains('show')) {
+    list.classList.remove('show');
   } else {
-    list.classList.add("show");
+    list.classList.add('show');
   }
 });
